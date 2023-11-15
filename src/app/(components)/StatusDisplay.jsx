e62854ec33705681dd84683d03c2729f0b4ed4b1
Arman7052 <99arman7052@gmail.com>
2023-11-15 22:45:27 +0600

    Ticket card design

---
+++ src/app/(components)/StatusDisplay.jsx
@@ -0,0 +1,11 @@
+const StatusDisplay = () => {
+  return (
+    <div className="p-1">
+      <span className="inline-block rounded-sm px-2 py-2 text-xs font-semibold text-gray-700 bg-green-200">
+        Done
+      </span>
+    </div>
+  );
+};
+
+export default StatusDisplay;
--- src/app/(components)/TicketCard.jsx
+++ src/app/(components)/TicketCard.jsx
@@ -1,13 +1,30 @@
 import DeleteBlock from "./DeleteBlock";
 import PriorityDisplay from "./PriorityDisplay";
 import ProgressDisplay from "./ProgressDisplay";
+import StatusDisplay from "./StatusDisplay";
 
 const TicketCard = () => {
   return (
-    <div>
-      <DeleteBlock></DeleteBlock>
-      <PriorityDisplay></PriorityDisplay>
-      <ProgressDisplay></ProgressDisplay>
+    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
+      <div className="flex m-3">
+        <PriorityDisplay></PriorityDisplay>
+        <div className="ml-auto">
+          <DeleteBlock></DeleteBlock>
+        </div>
+      </div>
+      <h4>Ticket Title</h4>
+      <hr className="h-px border-0 bg-page mb-2" />
+      <p className="whitespace-pre-wrap">This is the Ticket description .</p>
+      <div className="flex-grow"></div>
+      <div className="flex mt-2">
+        <div className="flex flex-col">
+          <p className="text-xs my-1">31/11/2023 10 pm</p>
+          <ProgressDisplay></ProgressDisplay>
+        </div>
+        <div className="ml-auto flex items-end">
+          <StatusDisplay></StatusDisplay>
+        </div>
+      </div>
     </div>
   );
 };
